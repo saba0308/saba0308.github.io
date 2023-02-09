8d57c03c1b1dfc06563459d92d1167238b4c4572
saba0308 <sabapathi030820@gmail.com>
2023-02-09 18:18:51 +0530

    2.0.7

--- src/app/components/admin/services/products/product.service.ts
+++ src/app/components/admin/services/products/product.service.ts
@@ -7,7 +7,8 @@ import { productData } from 'src/app/components/model/product';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiServer = "http://localhost:3000";
+  // https://template-json-server.vercel.app/api
+  private apiServer = "https://template-json-server.vercel.app/api";
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
